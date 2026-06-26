a36f308c03676195621dd065c89b15b57cb59c53
ChViv <168715281+ChViv@users.noreply.github.com>
2026-06-26 14:03:17 +0800

    new

---
+++ Game/main.cpp
@@ -0,0 +1,34 @@
+#include <All.h>
+#include <iostream>
+#include <string>
+
+// hello
+
+int main()
+{
+	int zzWindowDisplayHeight = 400;	// Originally 720
+	int zzWindowDisplayWidth = zzWindowDisplayHeight * 1.78;	// Originally 1280
+
+	try
+	{
+		jazzy::Game game({ {zzWindowDisplayWidth, zzWindowDisplayHeight}, jazzy::Logger::LogLevel::Info });
+		game.run();
+	}
+	catch (const std::runtime_error&)
+	{
+		return EXIT_FAILURE;
+	}
+	catch (const std::invalid_argument)
+	{
+		return EXIT_FAILURE;
+	}
+	catch (const std::exception&)
+	{
+		return EXIT_FAILURE;
+	}
+	catch (...)
+	{
+		return EXIT_FAILURE;
+	}
+	return 0;
+}
